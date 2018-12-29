77b765beaeebee5cac45088001b02fe9ec8cb8d0
Nathan DECHER <nathan.decher@gmail.com>
2018-12-29 14:34:57 +0100

    written lua wrapper around all written statements

--- src/lib.lua
+++ src/lib.lua
@@ -29,6 +29,8 @@ local function exec(sql)
 	end
 	return true
 end
+
+-- prepared statement execution
 local function getrow(stat, ...)
 	if not stat then
 		error "No statement given"
@@ -58,6 +60,80 @@ local function getrows(stat, ...)
 	end
 	stat:reset()
 	return rows
+end
+local function insert(stat, ...)
+	if not stat then
+		error "No statement given"
+	end
+	local a=stat:bind_values(...)
+	if a~=0 then
+		error("Error while binding values: "..a)
+	end
+	a=stat:step()
+	stat:reset()
+	if a~=sqlite.DONE then
+		error("Error while executing statement: "..a)
+	end
+	return stat:last_insert_rowid()
+end
+local function run(stat, ...)
+	if not stat then
+		error "No statement given"
+	end
+	local a=stat:bind_values(...)
+	if a~=0 then
+		error("Error while binding values: "..a)
+	end
+	a=stat:step()
+	stat:reset()
+	if a~=sqlite.DONE then
+		error("Error while executing statement: "..a)
+	end
+end
+
+-- function creation
+local function create(fn, stat, args, nsfwstat)
+	if not args then
+		if not nsfwstat then
+			-- simply exec the statement
+			return (function()
+				return fn(stat)
+			end)
+		else
+			-- exec a different statement if nsfw mode is on
+			return (function(nsfw)
+				return fn(nsfw and nsfwstat or stat)
+			end)
+		end
+	else
+		-- reverse arg list
+		local argmap={}
+		local argc=#args
+		for i, v in ipairs(args) do
+			argmap[v]=i
+		end
+		
+		if not nsfwstat then
+			-- translate map into list and exec statement
+			return (function(args)
+				local list={}
+				for arg, val in pairs(args) do
+					list[argmap[arg]]=val
+				end
+				print(table.unpack(list, #argmap))
+				return fn(stat, table.unpack(list, 1, argc))
+			end)
+		else
+			-- translate map into list and exec a different statement if nsfw mode is on
+			return (function(args, nsfw)
+				local list={}
+				for arg, val in pairs(args) do
+					list[argmap[arg]]=val
+				end
+				return fn(nsfw and nsfwstat or stat, table.unpack(list, 1, argc))
+			end)
+		end
+	end
 end
 
 -- library load
@@ -143,6 +219,48 @@ CREATE TEMP TABLE notags(
 			print("Unable to load SQL for "..name..": "..err)
 		end
 	end
+	
+	-- create basic lib functions
+	do
+		local stat=lib.stat
+		-- format getters
+		lib.getformats=create(getrows, stat.getformats)
+		lib.getformatbyid=create(getrow, stat.getformatbyid, {'id'})
+		lib.getformatbyname=create(getrow, stat.getformatbyname, {'name'})
+		
+		-- format adders
+		lib.addformat=create(insert, stat.addformat, {'name'})
+		
+		-- tag getters
+		lib.gettags=create(getrows, stat.gettags, nil, stat.getalltags)
+		lib.gettagbyid=create(getrow, stat.gettagbyid, {'id'})
+		lib.gettagbyname=create(getrow, stat.gettagbyname, {'name'})
+		lib.gettagsforimage=create(getrows, stat.gettagsforimage, {'image'})
+		lib.gettagsforalbum=create(getrows, stat.gettagsforalbum, {'album'})
+		
+		-- tag adders
+		lib.addtag=create(insert, stat.addtag, {'name', 'color'}, stat.addnsfwtag)
+		lib.addtagtoimage=create(insert, stat.addtagtoimage, {'tag', 'image'})
+		lib.addtagtoalbum=create(insert, stat.addtagtoalbum, {'tag', 'album'})
+		
+		-- album getters
+		lib.getalbums=create(getrows, stat.getalbums, nil, stat.getallalbums)
+		lib.getalbumbyid=create(getrow, stat.getalbumbyid, {'id'})
+		lib.getalbumbyname=create(getrow, stat.getalbumbyname, {'name'})
+		lib.getalbumsfortag=create(getrows, stat.getalbumsfortag, {'tag'}, stat.getallalbumsfortag)
+		lib.getalbumsforimage=create(getrows, stat.getalbumsforimage, {'tag'}, stat.getallalbumsforimage)
+		
+		-- album adders
+		lib.addalbum=create(insert, stat.addalbum, {'name'}, stat.addnsfwalbum)
+		lib.addimagetoalbum=create(insert, stat.addimagetoalbum, {'image', 'album', 'offset'})
+		
+		-- image getters
+		lib.getimages=create(getrows, stat.getimages, nil, stat.getallimages)
+		lib.getimagebyid=create(getrow, stat.getimagebyid, {'id'})
+		lib.getimagebyname=create(getrow, stat.getimagebyname, {'name'})
+		lib.getimagesfortag=create(getrows, stat.getimagesfortag, {'tag'}, stat.getallimagesfortag)
+		lib.getimagesforalbum=create(getrows, stat.getimagesforalbum, {'album'}, stat.getallimagesforalbum)
+	end
 end
 
 -- library finalize
@@ -166,46 +284,35 @@ function lib.getfilename(id, ext)
 	return config.imgdir..'/i'..id..'.'..ext
 end
 
--- get all the tags
-function lib.gettags(nsfw)
-	return getrows(nsfw and lib.stat.getalltags or lib.stat.gettags)
-end
--- get tag by id
-function lib.gettagbyid(id)
-	return getrow(lib.stat.gettagbyid, id)
-end
--- get tag by name
-function lib.gettagbyname(name)
-	return getrow(lib.stat.gettagbyname, name)
-end
--- get tags for image
-function lib.gettagsforimage(id)
-	return getrows(lib.stat.gettagsforimage, id)
-end
-
--- get formats
-function lib.getformats(id)
-	return getrows(lib.stat.getformats)
-end
--- get format by id
-function lib.getformatbyid(id)
-	return getrow(lib.stat.getformatbyid, id)
-end
--- get format by name
-function lib.getformatbyname(name)
-	return getrow(lib.stat.getformatbyname, name)
-end
-
--- get images
-function lib.getimages(nsfw)
-	return getrows(nsfw and lib.stat.getallimages or lib.stat.getimages)
-end
--- get images for tag
-function lib.getimagesfortag(tag, nsfw)
-	if type(tag)=='string' then
-		tag=lib.gettagbyname(tag).id
+-- get albums for tag combo
+function lib.getalbumsfortags(yes, no, nsfw)
+	run(stat.cleantags)
+	if yes then
+		for i, tag in ipairs(yes) do
+			run(stat.addyestag, tag)
+		end
 	end
-	return getrows(nsfw and lib.stat.getallimagesfortag or lib.stat.getimagesfortag, tag)
+	if no then
+		for i, tag in ipairs(no) do
+			run(stat.addnotag, tag)
+		end
+	end
+	return getrows(nsfw and stat.getallalbumsfortags or stat.getalbumsfortags)
+end
+-- get images for tag combo
+function lib.getimagesfortags(yes, no, nsfw)
+	run(stat.cleantags)
+	if yes then
+		for i, tag in ipairs(yes) do
+			run(stat.addyestag, tag)
+		end
+	end
+	if no then
+		for i, tag in ipairs(no) do
+			run(stat.addnotag, tag)
+		end
+	end
+	return getrows(nsfw and stat.getallimagesfortags or stat.getimagesfortags)
 end
 
 return lib
