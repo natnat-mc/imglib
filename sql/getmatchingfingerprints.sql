41acf711a7bc939b00e5373ea86bbfbc964523f8
DECHER Nathan <nathan.decher@gmail.com>
2019-01-02 23:43:01 +0100

    better fingerprint matcher

--- sql/getmatchingfingerprints.sql
+++ sql/getmatchingfingerprints.sql
@@ -1,6 +1,6 @@
 SELECT image, size, fingerprint, diff
 	FROM (
-		SELECT image, size, fingerprint, delta(fingerprint, :original) AS diff
+		SELECT image, size, fingerprint, imagedelta(fingerprint, :original) AS diff
 			FROM fingerprints
 		)
 	WHERE diff<:maxdelta
--- src/sqlfuncs.lua
+++ src/sqlfuncs.lua
@@ -5,7 +5,7 @@ local func={}
 function M.delta(ctx, a, b)
 	local l=#a
 	if l~=#b then
-		ctx:result_error "Blobs must have the same length"
+		return ctx:result_error "Blobs must have the same length"
 	end
 	local diff=0
 	for i=1, l do
@@ -16,9 +16,36 @@ function M.delta(ctx, a, b)
 		end
 		diff=diff+ld
 	end
-	ctx:result_number(diff/l/255)
+	return ctx:result_number(diff/l/255)
 end
 func.delta=2
+
+-- calculate the difference between two images
+local invsqrt3=1/math.sqrt(3)
+local function colordelta(r1, g1, b1, r2, g2, b2)
+	local r=(r1-r2)/255
+	local g=(g1-g2)/255
+	local b=(b1-b2)/255
+	local d=math.sqrt(r*r+g*g+b*b)*invsqrt3
+	return d
+end
+function M.imagedelta(ctx, a, b)
+	local len=#a/3
+	if len~=#b/3 then
+		return ctx:result_error "Blobs must have the same length"
+	end
+	if len%1~=0 then
+		return ctx:result_error "Blobs must have a length multiple of three"
+	end
+	local delta=0
+	for i=0, len-1 do
+		local r1, g1, b1=a:byte(len*3+1, len*3+3)
+		local r2, g2, b2=b:byte(len*3+1, len*3+3)
+		delta=delta+colordelta(r1, g1, b1, r2, g2, b2)
+	end
+	return ctx:result_number(delta/len)
+end
+func.imagedelta=2
 
 -- install all custom functions
 function M.install(db)
